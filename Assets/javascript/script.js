eea7e05b1b37190d2f8ebfc168b4b856b8894cdb
Kendall Arceo <arceokendall@gmail.com>
2020-02-23 02:01:29 -0600

    Used materialize on home profile portfolio.

---
+++ Assets/javascript/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ 
+    $(".button-collapse").sideNav();
+        
+});
+
+console.log(".button-collapse")
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
