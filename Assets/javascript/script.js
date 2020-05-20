2af9e88a63b3cc603462afa0be7223376f61cd0e
Kendall Arceo <arceokendall@gmail.com>
2020-05-19 19:01:16 -0500

    reverting back from project 2 portfolio, making another repository for the reacr portfolio.

---
+++ Assets/javascript/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ 
+    $(".button-collapse").sideNav();
+        
+});
+
+console.log(".button-collapse")
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+});
